ebdacc148fa00ec7a23d95eec8578b292a875372
[Minh] <[minhhoang120220@gmail.com]>
2025-02-19 16:09:51 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,69 +1,30 @@
 import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
-import React, { useRef, useState } from 'react';
 
 function App() {
-const [timer, setTimer] = useState(0);
-  const timer2 = useRef(0);
-  let a = 0;
-  let b = 0;
-
-  const handleClick = () => {
-    console.log('timer: ', timer);
-    console.log('timer2: ', timer2);
-    console.log('a: ', a);
-    console.log('b: ', b);
-  };
-
-  const handleIncrease = () => {
-    setTimer(timer + 1);
-    
-    a += 1;
-  };
-
-  const handleTimer2 = () => {
-    timer2.current += 1;
-    b += 1;
-  }
+  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
 
   return (
-    <div>
-      <p style={{ color: 'black'}}>Hello timer 1: {timer} </p>
-      <br/>
-      <p style={{ color: 'black'}}>Hello timer 2: {`${timer2.current}`}</p>
-      <br/>
-      <button onClick={handleClick}>Click</button>
-      <button onClick={handleIncrease}>Timer 1</button>
-      <button onClick={handleTimer2}>Timer 2</button>
+    <div className="App container">
+      <h1>Coder Timer</h1>
+      <div className="timer__wrapper">
+        <div className="timer__display">
+          <p>{formatTime(time)}</p>
+        </div>
+        <div className="button__wrapper">
+          <button className="button" onClick={stopTimer}>
+            Stop
+          </button>
+          <button className="button" ref={active} onClick={startTimer}>
+            Start
+          </button>
+          <button className="button" onClick={resetTimer}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
-//   const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
-
-//   return (
-//     <div className="App container">
-//       <h1>Coder Timer</h1>
-//       <div className="timer__wrapper">
-//         <div className="timer__display">
-//           <p>{formatTime(time)}</p>
-//         </div>
-//         <div className="button__wrapper">
-//           <button className="button" onClick={stopTimer}>
-//             Stop
-//           </button>
-//           <button
-//             className="button"
-//             ref={"Your code here"}
-//             onClick={startTimer}
-//           >
-//             Start
-//           </button>
-//           <button className="button" onClick={resetTimer}>
-//             Reset
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
 }
 
 export default App;
--- src/formatTime.js
+++ src/formatTime.js
@@ -1,9 +1,10 @@
 export const formatTime = (time) => {
-  const getSeconds = `0${time % 60}`.slice(-2);
-  const minutes = `${Math.floor(time / 60)}`;
-  const getMinutes = `0${minutes % 60}`.slice(-2);
-  const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
-
+  const seconds = time%60;
+  const getSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  const minutes = Math.floor(time/60)%60;
+  const getMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  const hours = Math.floor(time/3600);
+  const getHours = hours < 10 ? `0${hours}` : `${hours}`;
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
   
