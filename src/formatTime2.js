d092e3a3f334e1a016003a47a043e2b8c868df0f
Công Minh Hoàng <420ent.@Congs-MacBook-Air.local>
2025-02-19 15:44:18 +0700

    submit

--- src/formatTime2.js
+++ src/formatTime2.js
@@ -1,8 +1,19 @@
 export const formatTime = (time) => {
-  const getSeconds = "Your code here";
-  const minutes = "Your code here";
-  const getMinutes = "Your code here";
-  const getHours = "Your code here";
+  const getSeconds = `0${time % 60}`.slice(-2);
+  const minutes = `${Math.floor(time / 60)}`;
+  const getMinutes = `0${minutes % 60}`.slice(-2);
+  const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
 
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
+  
+export const formatTime2 = (time) => {
+  const seconds = time%60;
+  const displaySeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  const minutes = Math.floor(time/60)%60;
+  const displayMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  const hours = Math.floor(time/3600);
+  const displayHours = hours < 10 ? `0${hours}` : `${hours}`;
+  return `${displayHours} : ${displayMinutes} : ${displaySeconds}`;
+};
+
--- src/useTimer.js
+++ src/useTimer.js
@@ -4,24 +4,30 @@ const useTimer = (ini = 0) => {
   const [time, setTime] = useState(0);
 
   const isStart = useRef(true);
-  const active = useRef(true);
+  const active = useRef({disabled: false});
   const refInterval = useRef(0);
-
 
   const startTimer = () => {
     // "Your code here";
     refInterval.current = setInterval(() => {
       setTime(prevTime=>prevTime +1)
     }, 1000);
+    console.log(active);
     active.current.disabled = true;
+    isStart.current = true;
   };
+
   const stopTimer = () => {
     // "Your code here";
     clearInterval(refInterval.current);
+    isStart.current = false;
+    active.current.disabled = false;
   };
+
   const resetTimer = () => {
     // "Your code here";
-    // setTime(0);
+    setTime(0);
+    clearInterval(refInterval.current);
     active.current.disabled = false;
   };
 
