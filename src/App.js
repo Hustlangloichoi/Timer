4d113b485a2f300bbb920703f754d0f6b3480d87
Công Minh Hoàng <420ent.@Congs-MacBook-Air.local>
2025-02-19 15:38:56 +0700

    submit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,69 @@
-import "Your code here";
+import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
+import React, { useRef, useState } from 'react';
 
 function App() {
-  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+const [timer, setTimer] = useState(0);
+  const timer2 = useRef(0);
+  let a = 0;
+  let b = 0;
+
+  const handleClick = () => {
+    console.log('timer: ', timer);
+    console.log('timer2: ', timer2);
+    console.log('a: ', a);
+    console.log('b: ', b);
+  };
+
+  const handleIncrease = () => {
+    setTimer(timer + 1);
+    
+    a += 1;
+  };
+
+  const handleTimer2 = () => {
+    timer2.current += 1;
+    b += 1;
+  }
 
   return (
-    <div className="App container">
-      <h1>Coder Timer</h1>
-      <div className="timer__wrapper">
-        <div className="timer__display">
-          <p>{"Your code here"}</p>
-        </div>
-        <div className="button__wrapper">
-          <button className="button" onClick={"Your code here"}>
-            Stop
-          </button>
-          <button
-            className="button"
-            ref={"Your code here"}
-            onClick={"Your code here"}
-          >
-            Start
-          </button>
-          <button className="button" onClick={"Your code here"}>
-            Reset
-          </button>
-        </div>
-      </div>
+    <div>
+      <p style={{ color: 'black'}}>Hello timer 1: {timer} </p>
+      <br/>
+      <p style={{ color: 'black'}}>Hello timer 2: {`${timer2.current}`}</p>
+      <br/>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={handleIncrease}>Timer 1</button>
+      <button onClick={handleTimer2}>Timer 2</button>
     </div>
   );
+//   const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+
+//   return (
+//     <div className="App container">
+//       <h1>Coder Timer</h1>
+//       <div className="timer__wrapper">
+//         <div className="timer__display">
+//           <p>{formatTime(time)}</p>
+//         </div>
+//         <div className="button__wrapper">
+//           <button className="button" onClick={stopTimer}>
+//             Stop
+//           </button>
+//           <button
+//             className="button"
+//             ref={"Your code here"}
+//             onClick={startTimer}
+//           >
+//             Start
+//           </button>
+//           <button className="button" onClick={resetTimer}>
+//             Reset
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
 }
 
 export default App;
--- src/useTimer.js
+++ src/useTimer.js
@@ -1,24 +1,33 @@
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
+  const [time, setTime] = useState(0);
 
-  const isStart = "Your code here";
-  const active = "Your code here";
-  const refInterval = "Your code here";
+  const isStart = useRef(true);
+  const active = useRef(true);
+  const refInterval = useRef(0);
+
 
   const startTimer = () => {
-    "Your code here";
+    // "Your code here";
+    refInterval.current = setInterval(() => {
+      setTime(prevTime=>prevTime +1)
+    }, 1000);
     active.current.disabled = true;
   };
   const stopTimer = () => {
-    "Your code here";
+    // "Your code here";
+    clearInterval(refInterval.current);
   };
   const resetTimer = () => {
-    "Your code here";
+    // "Your code here";
+    // setTime(0);
     active.current.disabled = false;
   };
 
   return { time, startTimer, stopTimer, resetTimer, active };
 };
 export default useTimer;
+
+
+// dùng use ref ở đây để có thể chẹkc 
